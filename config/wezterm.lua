222bf582cb88430a0bb82f9f50db28adb866062b
Adonis Ling <adonis0147@gmail.com>
2024-07-16 14:34:19 +0800

    Update wezterm.lua

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.audible_bell = 'Disabled'
+
 config.enable_tab_bar = false
 
 config.color_scheme = 'Monokai (dark) (terminal.sexy)'
