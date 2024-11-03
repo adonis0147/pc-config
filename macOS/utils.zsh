850383813ab0af88fe1b23c32f2b832ce50cc802
Adonis Ling <adonis0147@gmail.com>
2024-11-03 10:44:10 +0800

    refactor: add auxiliary functions to macOS/utils.zsh

---
+++ macOS/utils.zsh
@@ -0,0 +1,41 @@
+function install_neovim() {
+	local url='https://api.github.com/repos/neovim/neovim/releases/latest'
+	local latest
+	local current
+
+	latest="$(curl -s "${url}" |
+		python -c "import json; import sys; print(json.load(sys.stdin)['tag_name'])")"
+
+	if [[ -z "${latest}" ]]; then
+		return
+	fi
+
+	current="$(nvim -version 2>/dev/null | sed -n 's/NVIM \(.*\)/\1/p')"
+
+	if [[ "${current}" != "${latest}" ]]; then
+		local bin="${HOME}/.local/bin"
+		local share="${HOME}/.local/share"
+		local prefix="${HOME}/.local/opt/neovim"
+		local download_url
+
+		rm -rf "${prefix}"
+
+		mkdir -p "${bin}"
+		mkdir -p "${prefix}"
+
+		download_url="https://github.com/neovim/neovim/releases/download/${latest}/nvim-macos-$(uname -m).tar.gz"
+		curl -L "${download_url}" -o - | tar -C "${prefix}" --strip-components=1 -zxf -
+
+		ln -snf "${prefix}/bin"/* "${bin}"/
+
+		local dir
+		for dir in $(find "${prefix}/share/man" -mindepth 1 -maxdepth 1); do
+			local destination
+			destination="${share}/man/$(basename "${dir}")"
+			mkdir -p "${destination}"
+			ln -snf "${dir}"/* "${destination}"/
+		done
+	else
+		echo -e "\033[35;1mThe version of Neovim is latest.\033[0m"
+	fi
+}
