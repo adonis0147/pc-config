fbb66e3829db05805419875cc8007c77bc74bec6
Adonis Ling <adonis0147@gmail.com>
2026-09-08 20:04:02 +0800

    feat: add pi-review

--- common/utils.zsh
+++ common/utils.zsh
@@ -190,6 +190,7 @@ function mihomo_choose_proxy() {
 function setup_pi_settings() {
 	local pi_settings="${HOME}/.pi/agent/settings.json"
 	local pi_plan_mode_settings="${HOME}/.pi/agent/pi-plan-mode.json"
+	local pi_review_settings="${HOME}/.pi/agent/pi-review.json"
 	local pi_settings_template="${PC_CONFIG_PATH}/config/pi/settings.json"
 	local pi_custom_settings="${PC_CONFIG_PATH}/config/pi/custom-settings.json"
 	local ponytail_config="${HOME}/.config/ponytail/config.json"
@@ -197,6 +198,7 @@ function setup_pi_settings() {
 	mkdir -p "${pi_settings:h}" "${ponytail_config:h}"
 	ln -snf "${PC_CONFIG_PATH}/config/pi/ponytail.json" "${ponytail_config}"
 	ln -f "${PC_CONFIG_PATH}/config/pi/pi-plan-mode.json" "${pi_plan_mode_settings}"
+	ln -snf "${PC_CONFIG_PATH}/config/pi/pi-review.json" "${pi_review_settings}"
 
 	if [[ -e "${pi_settings}" || -L "${pi_settings}" ]]; then
 		return
